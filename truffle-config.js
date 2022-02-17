b8f8b4c701dd672376792db847bf9fac5a2f6efe
Darren Tan <darren1tech@gmail.com>
2022-02-17 09:52:41 -0500

    update network deployement setup to testnet

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,14 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const { ROPSTEN_URL, KOVAN_URL, MNEMONIC } = process.env;
+const { INFURA_API_KEY, ROPSTEN_ENDPOINT_URL, KOVAN_ENDPOINT_URL} = process.env;
+//console.log("ropsten url is " + ROPSTEN_ENDPOINT_URL + INFURA_API_KEY);
+const KOVAN_INFURA_URL = KOVAN_ENDPOINT_URL + INFURA_API_KEY;
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -55,16 +62,28 @@ module.exports = {
     // from: <address>,        // Account to send txs from (default: accounts[0])
     // websocket: true        // Enable EventEmitter interface for web3 (default: false)
     // },
+    
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      //provider: () => new HDWalletProvider(MNEMONIC, ROPSTEN_URL),
+      provider: () => new HDWalletProvider(MNEMONIC, ROPSTEN_ENDPOINT_URL + INFURA_API_KEY),
+      network_id: 3,       // Ropsten's id    
+      gas: 3000000,
+      gasPrice: 10000000000
+      // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    kovan : {
+      //provider: () => new HDWalletProvider(MNEMONIC, KOVAN_URL),
+      provider: () => new HDWalletProvider(MNEMONIC, KOVAN_INFURA_URL),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
